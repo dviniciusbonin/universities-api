d5299de1e36cb580c51d747132becf4ccddb0723
Douglas Vinicius <vinibonin007@gmail.com>
2022-09-17 00:50:57 -0300

    feat: add setup mongodb and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL)],
 })
 export class AppModule {}
